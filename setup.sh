60c5829c26fd7c2a8d0a565d3ead3e3b81e3b018
Mesum Raza Hemani <mes.javacca@gmail.com>
2021-01-31 16:30:52 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,4 @@
-mkdir -p ~/.streamlit/
-
 pip install streamlit
 pip install git+https://github.com/deepset-ai/haystack.git
 
-echo "\
-[general]\n\
-email = \"juan.ciro@premexcorp.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+streamlit run --server.enableCORS false --server.port $PORT ResponsiveFrontEnd_Streamlit.py
